b6f06bbd6a7c0634aa946b6f6f65c93dfa7cb0af
sergioluisal <sergioluisal@gmail.com>
2025-10-16 11:21:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def index():
 #|                             Executar o App                              |
 #===========================================================================|
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
